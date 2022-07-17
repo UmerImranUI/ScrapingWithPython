a82e74b257fac57fad69b02dc6d2ef6029734bff
UmerImranUI <89452424+UmerImranUI@users.noreply.github.com>
2022-07-17 06:16:52 -0700

    Add files via upload

---
+++ ProxyByPass/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ProxibypsItem(scrapy.Item):
+    product_name=scrapy.Field()
+    product_author=scrapy.Field()
+    product_price=scrapy.Field()
+    product_img=scrapy.Field()
+    pass
